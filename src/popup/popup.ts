ed03951ccdff35319926d6d723d35f35351d59ce
Edwin Kofler <edwin@kofler.dev>
2022-09-02 02:19:50 -0700

    change: Improve extension
    - Add tool versions
    - Update dependencies
    - Improve options and popup pages

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,10 +1,1 @@
 import * as browser from 'webextension-polyfill'
-
-let port = browser.runtime.connectNative('dev.kofler.kaxon.native')
-port.onMessage.addListener((response) => {
-	console.info('got response:', response)
-})
-
-document.querySelector('.open')?.addEventListener('click', async (ev) => {
-	port.postMessage('open-kaxon')
-})
