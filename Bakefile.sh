47e57bcb89f2c9b609d3371303e09c63b369f8be
Edwin Kofler <edwin@kofler.dev>
2022-10-15 00:25:54 -0700

    meta: Add nightly release tasks

--- Bakefile.sh
+++ Bakefile.sh
@@ -1,10 +1,31 @@
 # shellcheck shell=bash
 
-task.build() {
-	rm -rf dist/*
-	PATH="$HOME/.local/state/woof/installs/nodejs/v18.7.0/files/bin:$PATH" yarn run parcel build --no-optimize src/manifest.json
-}
-
 task.dev() {
 	find -L . -ignore_readdir_race \( -iname 'node_modules' -o -iname 'dist' -o -iname 'out' -o -iname 'target' \) -prune -o -print | entr -c bake build
+}
+
+task.build() {
+	rm -rf dist/*
+	pnpm parcel build --no-optimize src/manifest.json
+}
+
+task.release-nightly() {
+	task.build
+
+	mkdir -p './output'
+	tar czf './output/build.tar.gz' './dist'
+
+	util.publish './output/build.tar.gz'
+}
+
+util.publish() {
+	local file="$1"
+	bake.assert_not_empty 'file'
+
+	local tag_name='nightly'
+	git tag -fa "$tag_name" -m ''
+	git push origin ":refs/tags/$tag_name"
+	git push --tags
+	gh release upload "$tag_name" "$file" --clobber
+	gh release edit --draft=false nightly
 }
